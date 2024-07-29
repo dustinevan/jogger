041eaaa98470cf4485cf5c97105662caecdb6fe9
Dustin Currie <dustinevancurrie@gmail.com>
2024-07-29 13:08:13 -0600

    Gen certs (#3)
    * Add RFD-1
    
    * Add Initial RFD
    * Ubuntu Dockerfile for cgroups
    * GRPC Service definition proto
    
    * Ignore Dockerfile
    
    * Add Docker related files to .gitignore as these are for local development
    
    * RFD-1 Changes
    
    * Add/Reword job_service.proto comments
    * Explain job statuses
    * Remove optional job_id naming
    * Change job representation from string to cmd and args
    * Reword jobs are just strings RFD section
    * Remove RFD section on job_id slugs
    * Clarify client-side env var support
    * Add supported server-side env vars
    * Remove unneeded and misplaced mutex confidence :)
    * Add Link to Security section for clarity
    * Improve example usage
    
    * Apply suggestions from code review
    
    RFD Suggestions
    * Clarify buf usage
    * Clarify wording on how the job args are split from the cli args.
    
    Co-authored-by: Trent Clarke <trent.clarke@gmail.com>
    
    * RFD-1 Rework
    
    * RFD-1 Remove Mermaid
    
    * RFD-1 Updates
    
    * Better Design For Closing the OutputStreamer
    * Clarify that jobs have internal contexts that are children of the server context
    * Combine Job instance creation and Job start into a single call.
    
    * RFD-1 cgroups-v2 Design
    
    * Makefile, Buf Configs, Code Gen
    
    * Initial Makefile
    * Buf configuration files
    * Ignore local development artifacts
    * Commit generated protobuf and grpc files
    
    * Update Makefile
    
    * Remove old copy-exports command
    * Add make tests
    
    * Generate Certificates
    
    * Generates the following:
        - a self-signed CA cert
        - a server key pair and CA signed cert for localhost:50051
        - a user key pair and CA signed cert for user1
    * Prints the exports needed for the CLI
    
    ---------
    
    Co-authored-by: Dustin Currie <dustin@ratio.tech>
    Co-authored-by: Trent Clarke <trent.clarke@gmail.com>

---
+++ cmd/tools/gencerts/main.go
@@ -0,0 +1,282 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+var certDir = "certs/"
+
+func main() {
+
+	if _, err := os.Stat(certDir); os.IsNotExist(err) {
+		os.Mkdir(certDir, 0755)
+	}
+
+	crt, key, certAbsPath := caCert()
+	serverCertAbsPath, serverKeyAbsPath := serverCert(crt, key)
+	clientCertAbsPath, clientKeyAbsPath := clientCert(crt, key)
+
+	fmt.Println("Certificates generated successfully.")
+	// Print exports needed for client and server
+	fmt.Printf(`
+    The CLI requires the following environment variables:
+	
+        export JOGGER_CA_CERT_FILE=%s
+        export JOGGER_USER_CERT_FILE=%s
+        export JOGGER_USER_KEY_FILE=%s
+
+    The server requires the following environment variables:
+
+        export JOGGER_CA_CERT_FILE=%s
+        export JOGGER_SERVER_CERT_FILE=%s
+        export JOGGER_SERVER_KEY_FILE=%s
+        export JOGGER_SERVER_PORT=%d
+
+    Optionally, for the CLI also set: 
+
+        export JOGGER_HOST=localhost:50051
+
+`, certAbsPath, clientCertAbsPath, clientKeyAbsPath, certAbsPath, serverCertAbsPath, serverKeyAbsPath, 50051)
+}
+
+var maxInt128 = new(big.Int).Lsh(big.NewInt(1), 128)
+
+func caCert() (cert *x509.Certificate, key *ecdsa.PrivateKey, certAbsPath string) {
+	// Generate a ECDSA P256 key pair
+	private, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		fmt.Printf("failed to generate ECDSA P256 key pair: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Generate a serial number for the certificate
+	serialNumber, err := rand.Int(rand.Reader, maxInt128)
+	if err != nil {
+		fmt.Printf("failed to generate serial number: %v\n", err)
+		os.Exit(1)
+	}
+
+	// A self-signed certificate must be marked as a CA, and have the digital signature and cert sign key usage bits set
+	certTemplate := x509.Certificate{
+		Subject:               pkix.Name{Organization: []string{"Jogger"}, CommonName: "localhost"},
+		Issuer:                pkix.Name{Organization: []string{"Jogger"}, CommonName: "localhost"},
+		SerialNumber:          serialNumber,
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(1, 0, 0),
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		IsCA:                  true,
+		BasicConstraintsValid: true,
+	}
+
+	// Create the self-signed CA certificate, the cert template is used as both the template and parent
+	certBytes, err := x509.CreateCertificate(rand.Reader, &certTemplate, &certTemplate, &private.PublicKey, private)
+	if err != nil {
+		fmt.Printf("failed to create self-signed CA certificate: %v\n", err)
+		os.Exit(1)
+	}
+
+	// This generation program creates client and server certs, so we need the CA cert and key later in the
+	// process, we parse this so that we can return it.
+	cert, err = x509.ParseCertificate(certBytes)
+	if err != nil {
+		fmt.Printf("failed to parse self-signed CA certificate: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Write the certificate and private key to files
+	certFile, err := os.Create("certs/ca_tls.crt")
+	if err != nil {
+		fmt.Printf("failed to create cert file: %v\n", err)
+		os.Exit(1)
+	}
+	defer certFile.Close()
+	if err := pem.Encode(certFile, &pem.Block{Type: "CERTIFICATE", Bytes: certBytes}); err != nil {
+		fmt.Printf("failed to write cert file: %v\n", err)
+		os.Exit(1)
+	}
+
+	keyFile, err := os.Create("certs/ca_tls.key")
+	if err != nil {
+		fmt.Printf("failed to create key file: %v\n", err)
+		os.Exit(1)
+	}
+	defer keyFile.Close()
+	keyBytes, err := x509.MarshalECPrivateKey(private)
+	if err != nil {
+		fmt.Printf("failed to marshal private key: %v\n", err)
+		os.Exit(1)
+	}
+	if err := pem.Encode(keyFile, &pem.Block{Type: "EC PRIVATE KEY", Bytes: keyBytes}); err != nil {
+		fmt.Printf("failed to write key file: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Return the absolute path of the certificate file
+	certAbsPath, err = filepath.Abs(certFile.Name())
+	if err != nil {
+		fmt.Printf("failed to get absolute path of cert file: %v\n", err)
+		os.Exit(1)
+	}
+	return cert, private, certAbsPath
+}
+
+func serverCert(caCert *x509.Certificate, caKey *ecdsa.PrivateKey) (keyAbsPath string, certAbsPath string) {
+	// Generate a ECDSA P256 key pair
+	private, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		fmt.Printf("failed to generate ECDSA P256 key pair: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Generate a serial number for the certificate
+	serialNumber, err := rand.Int(rand.Reader, maxInt128)
+	if err != nil {
+		fmt.Printf("failed to generate serial number: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Create a certificate template for the server
+	certTemplate := x509.Certificate{
+		Subject:               pkix.Name{Organization: []string{"Jogger"}, CommonName: "server1"},
+		Issuer:                pkix.Name{Organization: []string{"Jogger"}, CommonName: "localhost"},
+		SerialNumber:          serialNumber,
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(1, 0, 0),
+		KeyUsage:              x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		DNSNames:              []string{"localhost:50051"},
+	}
+
+	// Create the server certificate using the CA certificate and private key
+	certBytes, err := x509.CreateCertificate(rand.Reader, &certTemplate, caCert, &private.PublicKey, caKey)
+	if err != nil {
+		fmt.Printf("failed to create server certificate: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Write the certificate and private key to files
+	certFile, err := os.Create("certs/server1_tls.crt")
+	if err != nil {
+		fmt.Printf("failed to create cert file: %v\n", err)
+		os.Exit(1)
+	}
+	defer certFile.Close()
+	if err := pem.Encode(certFile, &pem.Block{Type: "CERTIFICATE", Bytes: certBytes}); err != nil {
+		fmt.Printf("failed to write cert file: %v\n", err)
+		os.Exit(1)
+	}
+
+	keyFile, err := os.Create("certs/server1_tls.key")
+	if err != nil {
+		fmt.Printf("failed to create key file: %v\n", err)
+		os.Exit(1)
+	}
+	defer keyFile.Close()
+	keyBytes, err := x509.MarshalECPrivateKey(private)
+	if err != nil {
+		fmt.Printf("failed to marshal private key: %v\n", err)
+		os.Exit(1)
+	}
+	if err := pem.Encode(keyFile, &pem.Block{Type: "EC PRIVATE KEY", Bytes: keyBytes}); err != nil {
+		fmt.Printf("failed to write key file: %v\n", err)
+		os.Exit(1)
+	}
+	// Return the absolute path of the certificate and key files
+	certAbsPath, err = filepath.Abs(certFile.Name())
+	if err != nil {
+		fmt.Printf("failed to get absolute path of cert file: %v\n", err)
+		os.Exit(1)
+	}
+	keyAbsPath, err = filepath.Abs(keyFile.Name())
+	if err != nil {
+		fmt.Printf("failed to get absolute path of key file: %v\n", err)
+		os.Exit(1)
+	}
+	return certAbsPath, keyAbsPath
+}
+
+func clientCert(caCert *x509.Certificate, caKey *ecdsa.PrivateKey) (certAbsPath string, keyAbsPath string) {
+	// Generate a ECDSA P256 key pair
+	private, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		fmt.Printf("failed to generate ECDSA P256 key pair: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Generate a serial number for the certificate
+	serialNumber, err := rand.Int(rand.Reader, maxInt128)
+	if err != nil {
+		fmt.Printf("failed to generate serial number: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Create a certificate template for the client
+	certTemplate := x509.Certificate{
+		Subject:               pkix.Name{Organization: []string{"Jogger"}, CommonName: "user1"},
+		Issuer:                pkix.Name{Organization: []string{"Jogger"}, CommonName: "localhost"},
+		SerialNumber:          serialNumber,
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(1, 0, 0),
+		KeyUsage:              x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		BasicConstraintsValid: true,
+	}
+
+	// Create the client certificate using the CA certificate and private key
+	certBytes, err := x509.CreateCertificate(rand.Reader, &certTemplate, caCert, &private.PublicKey, caKey)
+	if err != nil {
+		fmt.Printf("failed to create client certificate: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Write the certificate and private key to files
+	certFile, err := os.Create("certs/user1_tls.crt")
+	if err != nil {
+		fmt.Printf("failed to create cert file: %v\n", err)
+		os.Exit(1)
+	}
+	defer certFile.Close()
+	if err := pem.Encode(certFile, &pem.Block{Type: "CERTIFICATE", Bytes: certBytes}); err != nil {
+		fmt.Printf("failed to write cert file: %v\n", err)
+		os.Exit(1)
+	}
+
+	keyFile, err := os.Create("certs/user1_tls.key")
+	if err != nil {
+		fmt.Printf("failed to create key file: %v\n", err)
+		os.Exit(1)
+	}
+	defer keyFile.Close()
+	keyBytes, err := x509.MarshalECPrivateKey(private)
+	if err != nil {
+		fmt.Printf("failed to marshal private key: %v\n", err)
+		os.Exit(1)
+	}
+	if err := pem.Encode(keyFile, &pem.Block{Type: "EC PRIVATE KEY", Bytes: keyBytes}); err != nil {
+		fmt.Printf("failed to write key file: %v\n", err)
+		os.Exit(1)
+	}
+	// Return the absolute path of the certificate and key files
+	certAbsPath, err = filepath.Abs(certFile.Name())
+	if err != nil {
+		fmt.Printf("failed to get absolute path of cert file: %v\n", err)
+		os.Exit(1)
+	}
+	keyAbsPath, err = filepath.Abs(keyFile.Name())
+	if err != nil {
+		fmt.Printf("failed to get absolute path of key file: %v\n", err)
+		os.Exit(1)
+	}
+	return certAbsPath, keyAbsPath
+}
